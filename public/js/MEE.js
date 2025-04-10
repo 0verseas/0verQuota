ceb2ca7320e16aa16fc9e3001bc75f04c64404fc
ricklai82 <ricklai82@gmail.com>
2025-04-10 14:09:04 +0800

    臺灣高等教育展頁面修改
    表格標頭凍結
    新增篩選與搜尋重置按鈕
    placeholder文字格式修改 加上RWD

--- public/js/MEE.js
+++ public/js/MEE.js
@@ -4,14 +4,17 @@
 */
 
 const $expositionList = $('#exposition-list');
-const $filterInput = $('#filter'); // 搜尋欄
-const $filterBtn = $('#filter-btn'); // 搜尋按鈕
+const $filterInput = $('#filter'); // 篩選欄
+const $filterBtn = $('#filter-btn'); // 篩選按鈕
+const $filterResetBtn = $('#filter-reset-btn'); // 篩選重置按鈕
 const $searchInput = $('#search'); // 搜尋欄
 const $searchBtn = $('#search-btn'); // 搜尋按鈕
+const $searchResetBtn = $('#search-reset-btn'); // 搜尋重置按鈕
 const $subTitle = $('.sub-title');
 
 const $titleArray = ['永平','巴生','吉隆坡','雙溪大年','亞庇','美里'];
 let stage = 1;
+let isInit = false; // 避免有人都重置了還一直按
 
 /**
 *	init
@@ -23,11 +26,29 @@ _init();
 *	bind event
 */
 
-$searchBtn.on('click', getExpositionListWithKeyword); // 列表搜尋更新
-$searchInput.keypress((e) => { e.keyCode == 13 && getExpositionListWithKeyword(); }); // 偵測是否在輸入欄按下 enter
+$searchBtn.on('click', _getExpositionListWithKeyword); // 列表搜尋按鈕事件
 
-$filterBtn.on('click', _filterInput); // 列表篩選
-$filterInput.keypress((e) => { e.keyCode == 13 && _filterInput(); }); // 偵測是否在輸入欄按下 enter
+$filterBtn.on('click', _filterInput); // 列表篩選按鈕事件
+
+// 偵測是否在搜尋輸入欄按下 enter
+$searchInput.on('keydown', (e)=>{
+    if(e.key === 'Enter'){
+        e.preventDefault(); // 阻止換行
+        _getExpositionListWithKeyword();
+    }
+})
+
+// 偵測是否在篩選輸入欄按下 enter
+$filterInput.on('keydown', (e)=>{
+    if(e.key === 'Enter'){
+        e.preventDefault(); // 阻止換行
+        _filterInput();
+    }
+})
+
+$searchResetBtn.on('click', _handleSearchReset); // reset 搜尋結果
+
+$filterResetBtn.on('click', _handleFileterReset); // reset 篩選結果
 
 async function _init() {
     loading.start();
@@ -35,7 +56,7 @@ async function _init() {
     stage = params.get('stage') && params.get('stage').length !== 0 ? params.get('stage') : 1;
     $subTitle.text($titleArray[stage-1]);
     try {
-        const response = await getExpositionList();
+        const response = await _getExpositionList();
         if (!response.ok) { throw response; }
         const schoolDatas = await response.json();
 
@@ -68,6 +89,7 @@ async function _init() {
             html += htmlLine;
         }
         await $expositionList.html(html);
+        isInit = true;
         await loading.complete();
     } catch (e) {
         console.error(e);
@@ -76,6 +98,7 @@ async function _init() {
         })
         await loading.complete();
     }
+    return;
 }
 
 function shuffle(array) {
@@ -83,6 +106,7 @@ function shuffle(array) {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
 }
 
 function _filterInput(){
@@ -98,13 +122,13 @@ function _filterInput(){
 }
 
 
-function getExpositionList() {
+function _getExpositionList() {
     return fetch(`${env.baseUrl}/malaysia-exposition/${stage}/school-list`, {
         credentials: 'include'
     });
 }
 
-async function getExpositionListWithKeyword() {
+async function _getExpositionListWithKeyword() {
     let keyword = $searchInput.val().toLowerCase();
     const converter = OpenCC.Converter({ from: 'cn', to: 'tw' });
     keyword = converter(keyword);
@@ -128,6 +152,9 @@ async function getExpositionListWithKeyword() {
             for (let schooldata of schoolDatas) {
                 if(!schooldata[3]){
                     schooldata[3] = '';
+                }
+                if(+schooldata[0]<10){
+                    schooldata[0] = '0'+schooldata[0];
                 }
                 htmlArray[count] = `
                     <tr>
@@ -153,6 +180,7 @@ async function getExpositionListWithKeyword() {
             html = '<tr><td colspan=16>無符合關鍵字的系所</td></tr>';
         }
         await $expositionList.html(html);
+        isInit = false;
         await loading.complete();
     } catch (e) {
         console.error(e);
@@ -162,5 +190,25 @@ async function getExpositionListWithKeyword() {
         await loading.complete();
     }
 
+    return;
+}
+
+function _handleFileterReset (){
+    $filterInput.val('');
+    value = '';
+
+    $expositionList.find('tr').filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+
+    return;
+}
+
+function _handleSearchReset(){
+    if(isInit){
+        return;
+    }
+    $searchInput.val('');
+    _init();
     return;
 }
